6628dead8e59832922c69a19e270a3e716101ad7
Pham Hong Quang 20163316 <quang.ph163316@sis.hust.edu.vn>
2020-12-16 12:08:21 +0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# JapaneseOCR
+# Flutter Tensorflow Lite Digit Recognizer
+Digit Recognizer using Tensorflow Lite and MNIST Data
+(It works on both Android and iOS.)
+
+<img src="./readme/1.png" width="250"/> <img src="./readme/2.png" width="250"/> <img src="./readme/3.png" width="250"/>
+
+#### Kaggle
+- https://bit.ly/3g7jfTL
+
+### Package
+tflite : A Flutter plugin for accessing TensorFlow Lite API
+
+https://pub.dev/packages/tflite
+
+
+### Youtube
+[![Flutter Github TFLite](./readme/youtube.png)](https://youtu.be/SRbxEQ89IBs)
+
+
+### More
+- [flutter_tflite_style_transfer](https://github.com/PuzzleLeaf/flutter_tflite_style_transfer)
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:mnistdigitrecognizer/screens/draw_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mnist Digit Recognizer',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          color: Colors.black,
+        ),
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: Colors.black,
+        ),
+      ),
+      home: DrawScreen(),
+    );
+  }
+}
