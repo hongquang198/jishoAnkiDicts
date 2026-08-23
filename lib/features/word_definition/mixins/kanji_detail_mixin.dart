c039275e2291433345c9a15fb17dc6703f702372
Quang Pham <hongquang127@gmail.com>
2026-08-23 20:15:47 +0700

    Fix: Han viet bottomsheet not fully displayed with android split screen mode

--- lib/features/word_definition/mixins/kanji_detail_mixin.dart
+++ lib/features/word_definition/mixins/kanji_detail_mixin.dart
@@ -12,165 +12,168 @@ mixin KanjiDetailMixin {
       builder: (context) => Material(
         child: Container(
           color: Colors.black87,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    Card(
-                      margin: EdgeInsets.symmetric(
-                        vertical: 30.0,
-                        horizontal: 5.0,
-                      ),
-                      child: Text(
-                        kanji.kanji ?? '',
-                        style: TextStyle(
-                          fontSize: 120.0,
+          child: SingleChildScrollView(
+            controller: ModalScrollController.of(context),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Card(
+                        margin: EdgeInsets.symmetric(
+                          vertical: 30.0,
+                          horizontal: 5.0,
                         ),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 20.0,
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          children: [
-                            Card(
-                              margin: EdgeInsets.only(
-                                top: 15.0,
-                                bottom: 5.0,
-                              ),
-                              child: Text(
-                                "N${kanji.jlpt ?? '0'}",
-                                style: TextStyle(
-                                  fontSize: 16.0,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                            Card(
-                              margin: EdgeInsets.only(
-                                top: 15.0,
-                                bottom: 5.0,
-                                left: 3.0,
-                              ),
-                              child: Text(
-                                "Cấp độ ${kanji.jouYou ?? '0'}",
-                                style: TextStyle(
-                                  fontSize: 16.0,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                        Card(
-                          child: Text(
-                            "${kanji.strokeCount ?? '0'} nét",
-                            style: TextStyle(
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.bold,
-                            ),
+                        child: Text(
+                          kanji.kanji ?? '',
+                          style: TextStyle(
+                            fontSize: 120.0,
                           ),
                         ),
-                      ],
-                    ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Text(
-                      'Hán việt: ',
-                      style: TextStyle(
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
                       ),
-                    ),
-                    Card(
-                      child: Text(
-                        kanji.hanViet ?? '',
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Card(
+                                margin: EdgeInsets.only(
+                                  top: 15.0,
+                                  bottom: 5.0,
+                                ),
+                                child: Text(
+                                  "N${kanji.jlpt ?? '0'}",
+                                  style: TextStyle(
+                                    fontSize: 16.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                              Card(
+                                margin: EdgeInsets.only(
+                                  top: 15.0,
+                                  bottom: 5.0,
+                                  left: 3.0,
+                                ),
+                                child: Text(
+                                  "Cấp độ ${kanji.jouYou ?? '0'}",
+                                  style: TextStyle(
+                                    fontSize: 16.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Card(
+                            child: Text(
+                              "${kanji.strokeCount ?? '0'} nét",
+                              style: TextStyle(
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        'Hán việt: ',
                         style: TextStyle(
                           fontSize: 16.0,
                           fontWeight: FontWeight.bold,
+                          color: Colors.white,
                         ),
                       ),
-                    ),
-                  ],
-                ),
-                Divider(
-                  color: Colors.grey,
-                ),
-                Row(
-                  children: [
-                    Text(
-                      'Keyword: ',
-                      style: TextStyle(
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
+                      Card(
+                        child: Text(
+                          kanji.hanViet ?? '',
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
                       ),
-                    ),
-                    Card(
-                      child: Text(
-                        kanji.keyword ?? '',
+                    ],
+                  ),
+                  Divider(
+                    color: Colors.grey,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        'Keyword: ',
                         style: TextStyle(
                           fontSize: 16.0,
                           fontWeight: FontWeight.bold,
+                          color: Colors.white,
                         ),
                       ),
+                      Card(
+                        child: Text(
+                          kanji.keyword ?? '',
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Divider(
+                    color: Colors.grey,
+                  ),
+                  Text(
+                    "Âm Onyomi: ${kanji.onYomi ?? ''}",
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
                     ),
-                  ],
-                ),
-                Divider(
-                  color: Colors.grey,
-                ),
-                Text(
-                  "Âm Onyomi: ${kanji.onYomi ?? ''}",
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
                   ),
-                ),
-                Divider(
-                  color: Colors.grey,
-                ),
-                Text(
-                  "Âm Kunyomi: ${kanji.kunYomi ?? ''}",
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
+                  Divider(
+                    color: Colors.grey,
                   ),
-                ),
-                Divider(
-                  color: Colors.grey,
-                ),
-                Text(
-                  "Ví dụ: ${kanji.readingExamples ?? ''}",
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
+                  Text(
+                    "Âm Kunyomi: ${kanji.kunYomi ?? ''}",
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
                   ),
-                ),
-                Divider(
-                  color: Colors.grey,
-                ),
-                Text(
-                  "Mẹo nhớ : ${kanji.koohiiStory1 ?? 'Chưa có'}",
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
+                  Divider(
+                    color: Colors.grey,
                   ),
-                ),
-              ],
+                  Text(
+                    "Ví dụ: ${kanji.readingExamples ?? ''}",
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Divider(
+                    color: Colors.grey,
+                  ),
+                  Text(
+                    "Mẹo nhớ : ${kanji.koohiiStory1 ?? 'Chưa có'}",
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
